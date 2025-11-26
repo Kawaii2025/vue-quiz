5dbb8bdefd0b1bbadb4944b0b0d8d494f5585e1e
User <user@example.com>
2025-11-26 15:26:21 +0800

    Fix tailwind config - add content paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
